32d7c500ac389aa9c8fc0985b771a29022c757b5
Nabel Mohammed <nabelahmedmohammed@gmail.com>
2024-11-29 20:57:40 +1100

    Added Week 3 code

---
+++ WEEK-3/public/js/extra.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $('.modal').modal();
+  
+    $('#button1').click(function () {
+      $(this).css('background-color', 'yellow'); // Change background to yellow
+    });
+  });
+  
